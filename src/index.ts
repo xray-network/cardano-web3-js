78f3fcaeba5985b32b1333e92492d91162c489e3
dipsy-hash <dipsy@xray.app>
2025-05-29 20:16:07 +0300

    Bip39 license, version bump

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export { CardanoWeb3 } from "@/core/cw3"
 export type * as CW3Types from "@/types"
 
-export { CML, MSL, UPLC } from "cardano-wasm-libs/browser"
+export { CML, MSL, UPLC } from "cardano-wasm-libs/nodejs"
 export { Data as PlutusData, Constr as PlutusConstr } from "@/libs/plutusData"
 export { Message } from "@/libs/messageSigning"
 export { default as utils } from "@/utils"
